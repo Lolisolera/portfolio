637555fb3ada7d7144a7ff3f0e7c21fd14cad403
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2024-09-18 16:10:12 +0100

    Update index.js

--- dice-game/index.js
+++ dice-game/index.js
@@ -1,7 +1,7 @@
 
 let randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-let randomDiceImage = "./dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+let randomDiceImage = "./dice" + randomNumber1 + ".png"; //dice1.png - dice6.png 
 
 let randomImageSource = "./images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
